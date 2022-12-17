4022da4ca73aae402b85e0bed3d6e18aa1e30735
DEBAMRITA11 <amritadeb440@gmail.com>
2022-12-17 22:55:51 +0600

    class-one

---
+++ script.js
@@ -0,0 +1,13 @@
+
+   // first-class
+   //let life = "beautiful";
+   // if ("Life === beautiful") alert ("Life is beautiful");
+   // console.log (5+10+5);
+   
+   //let firstname="Asif"
+   //let lastname="Akbar"
+   //console.log (firstname);
+   //console.log (lastname);
+
+   //console.log (10000);
+  // console.log ("Rahim");
