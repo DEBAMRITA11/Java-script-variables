20cdc4073cb038b09c8f18670c3abcbe3f1da96f
DEBAMRITA11 <amritadeb440@gmail.com>
2022-12-19 15:21:57 +0600

    class-one-completed

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 
-   // first-class
-   //let life = "beautiful";
-   // if ("Life === beautiful") alert ("Life is beautiful");
-   // console.log (5+10+5);
+    // java-script variables 
+    let  life = "beautiful";
+    if ("Life === beautiful") alert ("Life is beautiful");
+    console.log (5+10+5);
    
-   //let firstname="Asif"
-   //let lastname="Akbar"
-   //console.log (firstname);
-   //console.log (lastname);
-
-   //console.log (10000);
-  // console.log ("Rahim");
+   let firstname ="Asif"
+   let lastname ="Akbar"
+   console.log (firstname);
+   console.log (lastname);
+   // never use uppercase words and use piramide style words
+   console.log (10000);
+   console.log ("Rahim");
